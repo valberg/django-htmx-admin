fdc952924365ade4d7fcadcf914a869341c46c4d
Víðir Valberg Guðmundsson <valberg@orn.li>
2023-10-18 23:00:51 +0200

    Only try to htmx searchbar if it is there.

--- project/htmx_admin/static/js/htmx_admin.js
+++ project/htmx_admin/static/js/htmx_admin.js
@@ -1,11 +1,15 @@
 function initHtmxAdmin(path) {
     document.addEventListener("DOMContentLoaded", function (event) {
+        // If there is a searchbar, make it htmx
         const searchbar = document.getElementById("searchbar");
-        searchbar.setAttribute("hx-get", path);
-        searchbar.setAttribute("hx-target", "#changelist-form");
-        searchbar.setAttribute("hx-select", "#changelist-form");
-        searchbar.setAttribute("hx-push-url", "true");
-        searchbar.setAttribute("hx-trigger", "keyup changed delay:500ms");
-        searchbar.setAttribute("autocomplete", "off");
+        if (searchbar) {
+            searchbar.setAttribute("hx-get", path);
+            searchbar.setAttribute("hx-target", "#changelist-form");
+            searchbar.setAttribute("hx-select", "#changelist-form");
+            searchbar.setAttribute("hx-push-url", "true");
+            searchbar.setAttribute("hx-trigger", "keyup changed delay:500ms");
+            searchbar.setAttribute("autocomplete", "off");
+        }
     });
+
 }
